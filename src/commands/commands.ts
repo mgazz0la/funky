d9f2126fc89c66e01d1263c7cf983d59eebfe378
Michael Gazzola <michaelagazzola@gmail.com>
2021-11-27 13:57:30 -0500

    Fix misspelled filename

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -2,7 +2,7 @@ import { CommandHandler } from "./command-handler";
 import { PauseCommandHandler } from "./pause-command-handler";
 import { PlayCommandHandler } from "./play-command-handler";
 import { PogCommandHandler } from "./pog-command-handler";
-import { UnpauseCommandHandler } from "./unpaused-command-handler";
+import { UnpauseCommandHandler } from "./unpause-command-handler";
 
 export const COMMANDS: CommandHandler[] = [
   PogCommandHandler,
